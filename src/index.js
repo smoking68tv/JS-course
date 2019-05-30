7ddaccc2e5675e12f7533a387c853b59982e4354
Alexey <100.1000@list.ru>
2019-05-30 22:11:17 +0500

    tasks 1-4 completed

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
+
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i], i, array);
+    }
 }
 
 /*
@@ -16,6 +20,13 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
+    let Arr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        Arr[i] = fn(array[i], i, array);
+    }
+
+    return Arr;
 }
 
 /*
@@ -25,6 +36,19 @@ function map(array, fn) {
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 function reduce(array, fn, initial) {
+    let length = array.length, 
+        prevValue = initial;
+
+    for (let i = 0; i < length; i++) {
+        if (initial == undefined && i == 0) {
+            prevValue = array[0];
+            i++;
+        }
+            
+        prevValue = fn(prevValue, array[i], i, array);
+    }
+
+    return prevValue;
 }
 
 /*
@@ -36,6 +60,17 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+    let Arr = [], 
+        count = 0;
+    
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            Arr[count] = key.toUpperCase();
+            count++;
+        }
+    }
+
+    return Arr;
 }
 
 /*
@@ -45,6 +80,32 @@ function upperProps(obj) {
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
 function slice(array, from, to) {
+    let Arr = [],
+        index = 0;
+
+    if (from > array.length) { // Если begin больше длины последовательности вернется пустой массив.
+        return Arr = [];
+    }
+    if (from == undefined) { // Если begin неопределен, slice() начинает работать с индекса 0.
+        for (let i = 0; i < array.length; i++) {
+            Arr[index] = array[i];
+            ++index;
+        }
+    }
+    if (from < 0) { // Если индекс отрицательный, begin указывает смещение от конца последовательности
+        for (let i = array.length; i >= from; i--) {
+            Arr[index] = array[i];
+            ++index
+        }
+    } else {
+        for (let i = from; i < array.length; i++) {
+            Arr[index] = array[i];
+            ++index;
+        }
+    }
+
+    // for(let i = 0; i < )
+    return Arr;
 }
 
 /*
